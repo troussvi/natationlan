00fd09d286e59e0b823da6900c335dbcc1b27b50
ClementScordia <clement.scordia@gmail.com>
2017-03-06 14:31:25 +0100

    maj code

--- www/application/views/liste_nageur.php
+++ www/application/views/liste_nageur.php
@@ -2,48 +2,26 @@
 <body>
 <section>
  
-    <div id="Liste">
-        <?php
-        $attributes = array('name' => 'select');
-        echo form_open('apprentis/lister',$attributes);?>
-            <select id="select_list_promo" onchange="valeur_select(this)">
-                <?php
-                if($promo != NULL):
-                foreach($promo as $r):?>
-                 
-                <option id="<?php echo $r->IDPromo;?>" selected="">
-                    <?php echo $r->Annee_pro;?>
-                </option>
-                 
-                <?php endforeach; endif;?>
-            </select>
-            <input id="choix" name="select_promo" type="hidden" value="">
-        <?php echo form_close();?>
-         
-        <h1>Liste des apprentis</h1>
+    <div id="Liste">    
+        <h1>Liste des nageurs </h1>
         <?php
         $attributes = array('name' => 'form');
-        echo form_open('apprentis/delete',$attributes);?>
-            <table id="table_list" class="list_apprentis">
+            <table id="table_list" class="list_nageurs">
                 <?php
-                if($apprentis != NULL):
-                foreach($apprentis as $r):?>
+                if($_nageurs != NULL):
+                foreach($_nageurs as $r):?>
                 <tr>
-                    <th class="td_apprentis">Nom</th>
-                    <th class="td_apprentis">Prenom</th>
-                    <th class="td_apprentis">E-mail</th>
+                    <th class="td_nageurs">Nom</th>
+                    <th class="td_nageurs">Prenom</th>
+                    <th class="td_nageurs">E-mail</th>
                 </tr>
-                <tr id="<?php echo $r->IDEtudiant;?>" class="defaut" onclick="SelectLigne(this)">
-                    <td class="td_apprentis"><?php echo $r->Nom_et;?></td>
-                    <td class="td_apprentis"><?php echo $r->prenom_et;?></td>
-                    <td class="td_apprentis"><?php echo $r->mail_et;?></td>
+                <tr id="<?php echo $r->idnageur;?>" class="defaut">
+                    <td class="td_nageurs"><?php echo $r->Nom;?></td>
+                    <td class="td_nageurs"><?php echo $r->Prenom;?></td>
                 </tr>
                  
                 <?php endforeach; endif;?>
             </table>
-            <input id="select_suppr" name="select_suppr" type="hidden" value="">
-            <input id="del" type="submit" value="supprimer">
-        <?php echo form_close();?>
     </div>
      
 </section>
